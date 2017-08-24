115f3b460d115081ccb5b689ad7a0ad83458d3c6
merdogan10 <mustafaerdogan1994@gmail.com>
2017-08-24 12:39:19 +0300

    add static to gunicorn

--- mybestdeal/mybestdeal/urls.py
+++ mybestdeal/mybestdeal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from bestdeal import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -13,3 +14,4 @@ urlpatterns = [
     url(r'^(?P<category_name>[A-Za-z &]+)/$',views.category_items,name='category_items'),
     url(r'^(?P<category_name>[A-Za-z &]+)/(?P<sub_category_name>[A-Za-z &]+)/$',views.category_sub_category_items,name='category_sub_category_items'),
 ]
+urlpatterns += staticfiles_urlpatterns()
